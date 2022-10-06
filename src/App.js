8f96c2a131b551c06aa9fc976ac0febab8d8535b
Oleksii Tatarenkov <tatarenkov.oleksii@gmail.com>
2022-10-06 10:01:07 +0300

    add text

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        SECOND TEST FOR GITHUB
         <a
           className="App-link"
           href="https://reactjs.org"
